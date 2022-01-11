87e79f5632adbc5c64ed1138f365d0a5bd35932f
poli <poliphonic@gmail.com>
2022-01-11 22:26:16 +0300

    11 Jan 2022

--- sum_of_the_biggest.py
+++ sum_of_the_biggest.py
@@ -11,8 +11,7 @@ def my_func(num1, num2, num3):
     num3: an integer
     return: A sum of two biggest numbers.
     """
-    seq = sorted([num1, num2, num3])
-    return sum(seq[1:])
+    return sum(sorted([num1, num2, num3])[1:])
 
 
 n1, n2, n3 = map(int, input('Введите три целых числа через пробел: ').split())
